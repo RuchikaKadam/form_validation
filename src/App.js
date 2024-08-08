9a276845a218625dda05dc489e1e8318fd911259
Ruchika Kadam <149236565+RuchikaKadam@users.noreply.github.com>
2024-08-08 09:01:39 +0000

    form validation using formik and yup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Registrationform from "./Registrationform";
-import "./styles.css";
+import "./styles.css"; //styles for the entire app in one place
 
 export default function App() {
   return (
